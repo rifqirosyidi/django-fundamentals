e51e27e1cbd75b9eeb81b043089c872700471dad
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-01-14 18:08:48 +0700

    signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone_number = models.IntegerField(default=0)
+
+
+def create_user_profile(sender, **kwargs):
+    if kwargs['created']:
+        UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_user_profile, User)
+
